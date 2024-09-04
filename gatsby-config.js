4fec8f3cca24254920c9672d73373ae3ec512b52
Pratik Pakhale <48487283+pratikpakhale@users.noreply.github.com>
2024-09-04 12:57:46 +0530

    fix: og image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Pratik Pakhale is an undergraduate student at IIIT Dharwad pursuing BTech in Computer Science and Engineering. He enojoy building & breaking things with JavaScript, and he also love to swim 🏊 and surf twitter 🐦.',
     siteUrl: 'https://pratikpakhale.tech', // no trailing slash
-    image: 'og.png', // Path to your image you placed in the 'static' folder
+    image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@_pratikpakhale',
   },
   plugins: [
